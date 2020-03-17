1e768bdbd603e5c3e0fc01152606ad1f8daca521
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-03-17 18:12:45 -0300

    Added Header/About/Add comps and routing

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -31,9 +31,14 @@ export class TodoService {
     return this.http.put(url, todo, httpOptions)
   }
 
-  //Delete on API
+  //Delete Todo
   deleteTodo(todo: Todo):Observable<Todo> {
     const url = `${this.todosUrl}/${todo.id}`
     return this.http.delete<Todo>(url, httpOptions)
+  }
+
+  //Add Todo
+  addTodo(todo: Todo):Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions)
   }
 }
